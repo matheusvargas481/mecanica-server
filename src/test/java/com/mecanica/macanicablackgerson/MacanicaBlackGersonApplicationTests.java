9005d06f127a723e6d402e2b59fd06b4204eea90
Matheus da Silva Loreto de Vargas <ilegra.vargas@hypeflame.com.br>
2023-05-21 18:39:28 -0300

    ajusta projeto

---
+++ src/test/java/com/mecanica/macanicablackgerson/MacanicaBlackGersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecanica.macanicablackgerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacanicaBlackGersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
